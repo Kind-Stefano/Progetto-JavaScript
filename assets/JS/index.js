160d4a11f8f8b932fa1ad2295aedb41e49acb675
Kind-Stefano <bernardistefano73@gmail.com>
2024-11-15 11:48:28 +0100

    tornato alla versione precedente

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,18 +1,3 @@
-document.getElementById('counter').innerHTML = `
-    <div class="father">
-        <img src="assets/img/counter.png" alt="">
-        <h2>Contiamo!</h2>
-
-        <div>
-            <button id="aumenta" type="tooltip" title="Aumenta">+</button>
-            <button id="riduci" type="tooltip" title="Riduci">-</button>
-        </div>
-
-        <h2 id="counterDisplay">0</h2>
-    </div>
-`;
-
-
 let count = 0;
 
 const counterDisplay = document.getElementById('counterDisplay');
